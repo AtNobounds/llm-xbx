657d8c89cb638fd3f62deb9b303dfdd57f67ed6f
AtNobounds <63623629+AtNobounds@users.noreply.github.com>
2025-07-03 19:00:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,3 @@
-from dotenv import find_dotenv, load_dotenv
-import os
-
-# 读取本地/项目的环境变量。
-
-# find_dotenv()寻找并定位.env文件的路径
-# load_dotenv()读取该.env文件，并将其中的环境变量加载到当前的运行环境中
-# 如果你设置的是全局的环境变量，这行代码则没有任何作用。
-_ = load_dotenv(find_dotenv())
-
-# 获取环境变量 API_KEY
-IFLYTEK_SPARK_APP_ID = os.environ["IFLYTEK_SPARK_APP_ID"]
-IFLYTEK_SPARK_API_KEY = os.environ["IFLYTEK_SPARK_API_KEY"]
-IFLYTEK_SPARK_API_SECRET = os.environ["IFLYTEK_SPARK_API_SECRET"]
-
 import streamlit as st
 from langchain_openai import ChatOpenAI
 import os
